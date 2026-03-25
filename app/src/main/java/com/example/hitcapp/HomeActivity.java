77abaff9a6b1005d109ad0c76f469dcd926deb72
LuciusANDLucius <nothingbylucius@gmail.com>
2026-03-25 08:59:53 +0700

    update

--- app/src/main/java/com/example/hitcapp/HomeActivity.java
+++ app/src/main/java/com/example/hitcapp/HomeActivity.java
@@ -1,6 +1,9 @@
 package com.example.hitcapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,19 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button guessBtn1 = findViewById(R.id.Backtomain);
+
+
+        guessBtn1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
