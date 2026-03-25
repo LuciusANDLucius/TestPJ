e8d9db3cb81c7b62d89eec67cd8860e7006da2a1
LuciusANDLucius <nothingbylucius@gmail.com>
2026-03-25 11:06:01 +0700

    update new for login!!!

--- app/src/main/java/com/example/hitcapp/MainActivity.java
+++ app/src/main/java/com/example/hitcapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button guessBtn1 = findViewById(R.id.guest);
         Button register = findViewById(R.id.register);
+        Button login = findViewById(R.id.login);
 
 
         guessBtn1.setOnClickListener(new View.OnClickListener()
@@ -48,6 +50,25 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v) {
+                EditText objEmail = findViewById(R.id.email);
+                String sEmail = objEmail.getText().toString();
+
+                EditText objPassword = findViewById(R.id.password);
+                String sPassword = objPassword.getText().toString();
+
+                    if(sEmail.equals("email@alo.co") && sPassword.equals("1234")) {
+                        Intent it = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(it);
+                    }
+                else {
+                        android.widget.Toast.makeText(MainActivity.this, "Invalid credentials!", android.widget.Toast.LENGTH_SHORT).show();
+                }
+
+            };
         });
     }
 }
